0d1d23c9865a2ba782471aa50b682774b400935c
Svyatoshenko "Megal" Misha <mikhail.svyatoshenko@umbrella-web.com>
2017-12-08 14:56:36 +0300

    Added UNLICENSE

--- TabbarOnTop/TopTabBarController.swift
+++ TabbarOnTop/TopTabBarController.swift
@@ -12,24 +12,30 @@ class TopTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
+	override func viewWillLayoutSubviews() {
+		super.viewWillLayoutSubviews()
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+		self.tabBar.invalidateIntrinsicContentSize()
+		self.tabBar.frame.origin.y = self.topLayoutGuide.length
+		updateTransluciency()
+	}
 
 }
+
+private extension TopTabBarController {
+
+	// Set the translucent property to NO then back to YES to
+	// force the UITabBar to reblur, otherwise part of the
+	// new frame will be completely transparent if we rotate
+	// from a landscape orientation to a portrait orientation.
+	func updateTransluciency() {
+		if self.tabBar.isTranslucent {
+			self.tabBar.isTranslucent = false
+			self.tabBar.isTranslucent = true
+		}
+	}
+
+}
+

806449b75ee72b72a4c67146b36fab27e058f286
Svyatoshenko "Megal" Misha <mikhail.svyatoshenko@umbrella-web.com>
2017-12-08 14:02:48 +0300

    Initial Commit

---
+++ TabbarOnTop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TabbarOnTop
+//
+//  Created by Svyatoshenko "Megal" Misha on 08/12/2017.
+//  Copyright © 2017 megal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
